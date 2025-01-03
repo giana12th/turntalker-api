8e7b9b51de9523559f2d5dbbc99aa5739d54ada6
giana12th <giana12th@giana.net>
2025-01-03 16:16:54 +0900

    CORSミドルウェアを追加

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from typing import List
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# 許可するオリジンを設定 
+origins = [ "http://localhost:3000" ]
+
+# CORSミドルウェアを追加
+app.add_middleware( 
+    CORSMiddleware, 
+    allow_origins=origins, # 許可するオリジンを設定（例：すべてのオリジンを許可） 
+)
 
 class Team(BaseModel):
     name: str
